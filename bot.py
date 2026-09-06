4d4efff32c5774cefbd8b9dba09ac3882714c88d
CuteDemonDjibril <147176989+CuteDemonDjibril@users.noreply.github.com>
2026-09-06 19:16:17 -0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -257,4 +257,4 @@ async def finalizar_partida(ctx, vencedor: discord.User, perdedor: discord.User)
 async def on_ready():
     print(f"🤖 Bot {bot.user.name} online e pronto para os duelos!")
 
-bot.run(os.environ.get("DISCORD-TOKEN"))
+bot.run(os.environ.get("DISCORD_TOKEN"))
